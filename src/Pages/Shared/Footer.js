11b17fd910745cdc384fff792b5cb246b4e17d33
Joy Chandra Mollik <xoycmollik913@gmail.com>
2021-10-19 23:59:20 +0600

    footer info updated

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -160,31 +160,10 @@ const Footer = () => {
 						to='/home'
 						className='flex title-font font-medium items-center md:justify-start justify-center text-gray-100'
 					>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							stroke='currentColor'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
-							viewBox='0 0 24 24'
-						>
-							<path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5' />
-						</svg>
-						<span className='ml-3 text-xl'>Tailblocks</span>
+						<span className='ml-3 text-xl'>Doctors</span>
 					</Link>
 					<p className='text-sm text-gray-200 sm:ml-6 sm:mt-0 mt-4'>
-						© 2020 Tailblocks —
-						<Link
-							to='/home'
-							href='https://twitter.com/knyttneve'
-							rel='noopener noreferrer'
-							className='text-gray-400 ml-1'
-							target='_blank'
-						>
-							@knyttneve
-						</Link>
+						Doctors portal
 					</p>
 					<span className='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'>
 						<Link to='/home' className='text-gray-200'>
